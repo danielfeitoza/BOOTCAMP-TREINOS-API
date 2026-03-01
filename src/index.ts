5abf875ef6ace2256601ac6e00a7fc447ec66b7e
Daniel Silva Feitoza <daniel.feitoz@gmail.com>
2026-03-01 18:59:54 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-const name = "";
+// Import the framework and instantiate it
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler() {
+  return { hello: "world!ddddd" };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
